702744e1c2c5efad9478c1bbadb419c2c9c403d1
hyeonky <hyeon2762@naver.com>
2025-03-30 01:20:47 +0900

    수정) 오픈 그래프 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,10 +9,10 @@ export const metadata = {
     title: 'NUDAKE • 조현경',
     description: '리뉴얼 사이트',
     type: 'website',
-    url: 'http://www.mysite.com/article/article1.html',
+    url: 'https://nudake.vercel.app/',
     images: [
       {
-        url: 'http://www.mysite.com/article/article1_featured_image.jpg',
+        url: 'https://private-user-images.githubusercontent.com/181046564/428330418-450a9f88-e9f6-45b8-8f1f-ba6f3222321b.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDMyNjUzNjgsIm5iZiI6MTc0MzI2NTA2OCwicGF0aCI6Ii8xODEwNDY1NjQvNDI4MzMwNDE4LTQ1MGE5Zjg4LWU5ZjYtNDViOC04ZjFmLWJhNmYzMjIyMzIxYi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMzI5JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDMyOVQxNjE3NDhaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0xM2U1Mzk3ZDY2Y2U4MTQ3ZTQ1NzdhMWFiZDZhODU3MmQxNGZkNjcwNThhMGY3MWFiNWQ3ZDE3NGY3NDlmMGViJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.13lRSZPJop_BmKJcyyFX-OVhslXVeAJRtUJgBUloO8Y',
       },
     ],
   },
@@ -20,7 +20,9 @@ export const metadata = {
     card: 'summary',
     title: 'NUDAKE • 조현경',
     description: '리뉴얼 사이트',
-    images: ['http://www.mysite.com/article/article1.html'],
+    images: [
+      'https://private-user-images.githubusercontent.com/181046564/428330418-450a9f88-e9f6-45b8-8f1f-ba6f3222321b.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDMyNjUzNjgsIm5iZiI6MTc0MzI2NTA2OCwicGF0aCI6Ii8xODEwNDY1NjQvNDI4MzMwNDE4LTQ1MGE5Zjg4LWU5ZjYtNDViOC04ZjFmLWJhNmYzMjIyMzIxYi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMzI5JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDMyOVQxNjE3NDhaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0xM2U1Mzk3ZDY2Y2U4MTQ3ZTQ1NzdhMWFiZDZhODU3MmQxNGZkNjcwNThhMGY3MWFiNWQ3ZDE3NGY3NDlmMGViJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.13lRSZPJop_BmKJcyyFX-OVhslXVeAJRtUJgBUloO8Y',
+    ],
     creator: 'NUDAKE • 조현경',
   },
 }
